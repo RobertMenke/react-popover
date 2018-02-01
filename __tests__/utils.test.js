2a028629ce3eac9c40ad2dc8e2c7bcbe8e2be6f8
Brent Hosie <brent.hosie@rainfocus.com>
2018-01-31 17:34:04 -0700

    Provide a utility function to grab an element from the DOM and return its boundingClientRect if available.

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,4 +1,6 @@
-import { isDOMRect } from './../src/utils'
+import React from 'react'
+import { renderIntoDocument } from 'react-dom/test-utils'
+import { isDOMRect, getBoundingClientRectForElement } from './../src/utils'
 
 describe('verifying dom rect', () => {
     test('isDOMRect returns false when no element is passed', () => {
@@ -30,32 +32,63 @@ describe('verifying dom rect', () => {
     })
 
     test('isDOMRect returns false when "top" is missing from the rect object', () => {
-      const rect = { left: 0, right: 0, bottom: 0 }
+      const rect = { left: 0, right: 0, bottom: 0 };
       expect(isDOMRect(rect)).toBe(false);
     })
 
     test('isDOMRect returns false when "left" is missing from the rect object', () => {
-      const rect = { top: 0, right: 0, bottom: 0 }
+      const rect = { top: 0, right: 0, bottom: 0 };
       expect(isDOMRect(rect)).toBe(false);
     })
 
     test('isDOMRect returns false when "right" is missing from the rect object', () => {
-      const rect = { top: 0, left: 0, bottom: 0 }
+      const rect = { top: 0, left: 0, bottom: 0 };
       expect(isDOMRect(rect)).toBe(false);
     })
 
     test('isDOMRect returns false when "bottom" is missing from the rect object', () => {
-      const rect = { top: 0, left: 0, right: 0 }
+      const rect = { top: 0, left: 0, right: 0 };
       expect(isDOMRect(rect)).toBe(false);
     })
 
     test('isDOMRect returns true for objects that mimic DOMRect', () => {
-      const rect = { top: 0, left: 0, right: 0, bottom: 0 }
+      const rect = { top: 0, left: 0, right: 0, bottom: 0 };
       expect(isDOMRect(rect)).toBe(true);
     })
 
     test('isDOMRect returns true for true DOMRect objects', () => {
       const el = document.createElement('div');
       expect(isDOMRect(el.getBoundingClientRect())).toBe(true);
+    })
+})
+
+describe('verifying getBoundingClientRectForElement', () => {
+    test('getBoundingClientRectForElement will return an empty object when the element is null', () => {
+      expect(getBoundingClientRectForElement(null)).toEqual({});
+    })
+
+    test('getBoundingClientRectForElement will return an empty object when the element is undefined', () => {
+      expect(getBoundingClientRectForElement(undefined)).toEqual({});
+    })
+
+    test('getBoundingClientRectForElement will return a valid DOMRect for native elements', () => {
+      const el = document.createElement('button');
+      const result = getBoundingClientRectForElement(el);
+
+      expect(isDOMRect(result)).toBe(true);
+    })
+
+    test('getBoundingClientRectForElement will return a valid DOMRect for React elements', () => {
+      // findDOMNode does not work on stateless functional components, so we have to pass a class
+      class SomeReactElement extends React.Component {
+        render() {
+          return <button>A button</button>
+        }
+      }
+
+      const element = renderIntoDocument(<SomeReactElement />);
+      const result = getBoundingClientRectForElement(element);
+
+      expect(isDOMRect(result)).toBe(true);
     })
 })
